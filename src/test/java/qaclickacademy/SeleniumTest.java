fb140eabc6411f9913e8ba3e3cd0c920d46c643b
varsha <varsha.reddy10@gmail.com>
2019-05-22 11:39:17 -0500

    first commit

---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void BrowserAUtomation()
+	{
+		System.out.println("BrowserAUtomation");
+	}
+ 
+	@Test
+	public void elementsUi()
+	{
+		System.out.println("elementsUi");
+	}
+}
